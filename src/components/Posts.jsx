178f86decd7e32082ec31dbfa15929901ff591fd
Ashen Thilakarathna <134621843+Ashein00@users.noreply.github.com>
2024-11-02 21:42:42 +0530

    post pages completed

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,57 +1,18 @@
 import React, { useState } from 'react'
 import PostItem from './PostItem'
-
-import Thumbnail1 from '../images/blog1.jpg'
-import Thumbnail2 from '../images/blog2.jpg'
-import Thumbnail3 from '../images/blog3.jpg'
-import Thumbnail4 from '../images/blog4.jpg'
-
-const DUMMY_POSTS = [
-  {
-    id: '1',
-    thumbnail: Thumbnail1,
-    category: 'Education',
-    title: 'How to learn React',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
-    authorID: 3
-  },
-  {
-    id: '2',
-    thumbnail: Thumbnail2,
-    category: 'Science',
-    title: 'How to learn Physics',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
-    authorID: 1
-  },
-  {
-    id: '3',
-    thumbnail: Thumbnail3,
-    category: 'Weather',
-    title: 'How to learn Weather',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
-    authorID: 13
-  },
-  {
-    id: '4',
-    thumbnail: Thumbnail4,
-    category: 'Farming',
-    title: 'How to learn Farming',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
-    authorID: '3'
-  },
-]
+import { DUMMY_POSTS } from '../data'
 
 const Posts = () => {
     const [posts, setPosts] = useState(DUMMY_POSTS)
   return (
     <section className='posts'>
-        <div className="container posts__container">
+       {posts.length > 0 ? <div className="container posts__container">
           {
               posts.map(({ id, thumbnail, category, title, desc, authorID }) => 
                 <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} description={desc} authorID={authorID} />
               )            
           }
-        </div>
+        </div> : <h2 className='center'>No posts found!</h2> }
     </section>
   )
 }
