4118d69382bcd095cbc9878d339166b2b09d6883
Ashen Thilakarathna <134621843+Ashein00@users.noreply.github.com>
2024-11-02 16:32:20 +0530

    Card category button fixed

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PostAuthor from './PostAuthor';
 
-const PostItem = ({ postID, category, title, description, thumbnail, authorID }) => {
+const PostItem = ({ postID, category, title, description, authorID, thumbnail }) => {
   return (
     <article className='post'>
         <div className='post__thumbnail'>
-            <img src={thumbnail} alt='thumbnail' />
+            <img src={thumbnail} alt={title} />
         </div>
         <div className="post__content">
             <Link to={`/posts/${postID}`}>
@@ -15,7 +15,7 @@ const PostItem = ({ postID, category, title, description, thumbnail, authorID })
             <p>{description}</p>
             <div className="post__footer">
                 <PostAuthor />
-                <Link to={`/categories/${category}`}>{category}</Link>
+                <Link to={`/posts/categories/${category}`} className='btn category'>{category}</Link>
             </div>
         </div>
     </article>
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -10,7 +10,7 @@ const DUMMY_POSTS = [
   {
     id: '1',
     thumbnail: Thumbnail1,
-    category: 'education',
+    category: 'Education',
     title: 'How to learn React',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
     authorID: 3
@@ -18,7 +18,7 @@ const DUMMY_POSTS = [
   {
     id: '2',
     thumbnail: Thumbnail2,
-    category: 'science',
+    category: 'Science',
     title: 'How to learn Physics',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
     authorID: 1
@@ -26,7 +26,7 @@ const DUMMY_POSTS = [
   {
     id: '3',
     thumbnail: Thumbnail3,
-    category: 'weather',
+    category: 'Weather',
     title: 'How to learn Weather',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
     authorID: 13
@@ -34,7 +34,7 @@ const DUMMY_POSTS = [
   {
     id: '4',
     thumbnail: Thumbnail4,
-    category: 'farming',
+    category: 'Farming',
     title: 'How to learn Farming',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.',
     authorID: '3'
