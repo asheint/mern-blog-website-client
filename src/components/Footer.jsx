e56a3545ababf65516a3436400f5549f70781341
Ashen Thilakarathna <134621843+Ashein00@users.noreply.github.com>
2024-11-02 14:19:09 +0530

    Footer structured

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,17 @@ const Footer = () => {
     <footer>
        <ul className='footer__categories'>
         <li><Link to="/posts/categories/Aggriculture">Aggriculture</Link></li>
-        <li><Link to="/posts/categories/Bussimess">Bussimess</Link></li>
+        <li><Link to="/posts/categories/Bussiness">Bussiness</Link></li>
         <li><Link to="/posts/categories/Education">Education</Link></li>
         <li><Link to="/posts/categories/Entertainment">Entertainment</Link></li>
         <li><Link to="/posts/categories/Art">Art</Link></li>
         <li><Link to="/posts/categories/Investment">Investment</Link></li>
         <li><Link to="/posts/categories/Weather">Weather</Link></li>
         <li><Link to="/posts/categories/Uncategorized">Uncategorized</Link></li>
-
        </ul>
+       <div className='footer__copyright'>
+        <small>&copy; 2024, All rights reserved</small>
+       </div>
     </footer>
   )
 }
