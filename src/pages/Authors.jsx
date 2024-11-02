a7cad16659a7d058a4825a58bb09612cf5e70e27
Ashen Thilakarathna <134621843+Ashein00@users.noreply.github.com>
2024-11-02 23:29:46 +0530

    Loging and Register pages done!

---
+++ src/pages/Authors.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Avatar1 from '../images/avatar1.jpg'
+import Avatar2 from '../images/avatar2.jpg'
+import Avatar3 from '../images/avatar3.jpg'
+import Avatar4 from '../images/avatar4.jpg'
+import Avatar5 from '../images/avatar5.jpg'
+
+const Authors = () => {
+    const [authors, setAuthors] = useState(authorsData)
+  return (
+    <section className="authors">
+        <div className="container authors__container">
+
+        </div>
+    </section>
+  )
+}
+
+export default Authors
