eb9bc5db0c9305776402603b64ea14f80af5fbc3
Ashen Thilakarathna <134621843+Ashein00@users.noreply.github.com>
2024-11-13 21:22:20 +0530

    Post author completed!

--- src/components/PostAuthor.jsx
+++ src/components/PostAuthor.jsx
@@ -1,31 +1,42 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import Avatar from '../images/avatar1.jpg'
 import axios from 'axios'
+import ReactTimeAgo from 'react-time-ago'
+import TimeAgo from 'javascript-time-ago'
+
+import en from 'javascript-time-ago/locale/en'
+import ru from 'javascript-time-ago/locale/ru'
+
+TimeAgo.addDefaultLocale(en)
+TimeAgo.addLocale(ru)
+
+const ASSEST_URL = process.env.REACT_APP_ASSETS_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const PostAuthor = ({authorID, createdAt}) => {
-  const [author, setAuthor] = React.useState({})
+  const [author, setAuthor] = useState({})
 
   useEffect(() => {
     const getAuthor = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/api/users/${authorID}`)
-        const data = await response.json()
-        console.log(data)
+        const response = await axios.get(`${BASE_URL}/users/${authorID}`)
+        setAuthor(response?.data)
       } catch (error) {
         console.log(error)
       }
     }
+
+    getAuthor()
   })
 
   return (
     <Link to={'/posts/users/sdfsdf'} className='post__author'>
       <div className="post__author-avatar">
-        <img src={Avatar} alt="avatar"/>
+        <img src={`${ASSEST_URL}/uploads/${author?.avatar}`} alt={authorID}/>
       </div>
       <div className="post__author-details">
-        <h5>John Doe</h5>
-        <small>2 h ago</small>
+        <h5>By: {author?.name}</h5>
+        <small><ReactTimeAgo date={new Date(createdAt)} locale='en-US' /></small>
       </div>
     </Link>
   )
