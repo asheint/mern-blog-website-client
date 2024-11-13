459167ec78020d112b7f64c2fe1d2abddd1a9a30
Ashen Thilakarathna <134621843+Ashein00@users.noreply.github.com>
2024-11-13 20:55:15 +0530

    Thumbnail issue fixed!

--- src/components/PostAuthor.jsx
+++ src/components/PostAuthor.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Avatar from '../images/avatar1.jpg'
+import axios from 'axios'
 
-const PostAuthor = ({createdAt}) => {
+const PostAuthor = ({authorID, createdAt}) => {
+  const [author, setAuthor] = React.useState({})
+
+  useEffect(() => {
+    const getAuthor = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/api/users/${authorID}`)
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  })
+
   return (
     <Link to={'/posts/users/sdfsdf'} className='post__author'>
       <div className="post__author-avatar">
         <img src={Avatar} alt="avatar"/>
       </div>
       <div className="post__author-details">
-        <h5>By: John Doe</h5>
-        <small>2 hours ago</small>
+        <h5>John Doe</h5>
+        <small>2 h ago</small>
       </div>
     </Link>
   )
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PostAuthor from './PostAuthor';
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const ASSEST_URL = process.env.REACT_APP_ASSETS_URL;
 
 const PostItem = ({ postID, category, title, description, authorID, thumbnail, createdAt }) => {
     const shortDescription = description.length > 145 ? description.substring(0, 145) + '...' : description;
     const postTitle = title.length > 30 ? title.substring(0, 30) + '...' : title;
+    const thumbnailUrl = `${ASSEST_URL}/uploads/${thumbnail}`;
+
   return (
     <article className='post'>
         <div className='post__thumbnail'>
-            <img src={`${BASE_URL}/uploads/${thumbnail}`} alt={title} />
-            console.log(`${BASE_URL}/uploads/${thumbnail}`);
-
+            <img src={thumbnailUrl} alt={title} />
         </div>
         <div className="post__content">
             <Link to={`/posts/${postID}`}>
