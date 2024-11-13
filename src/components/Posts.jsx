92c0def4c3d65fc2e3300280b69387f2dfccb956
Ashen Thilakarathna <134621843+Ashein00@users.noreply.github.com>
2024-11-13 15:05:07 +0530

    Post loaded and working on the error with the thumbnail

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,15 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import PostItem from './PostItem'
-import { DUMMY_POSTS } from '../data'
+import Loader from './Loader'
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const Posts = () => {
-    const [posts, setPosts] = useState(DUMMY_POSTS)
+    const [posts, setPosts] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    
+    useEffect(() => {
+      const fetchPosts = async () => {
+        setIsLoading(true)
+        try {
+          const response = await axios.get(`${BASE_URL}/posts`)
+          setPosts(response?.data)
+        } catch (error) {
+          console.log(error)
+        }
+
+        setIsLoading(false)
+      }
+      fetchPosts();
+    },[])
+
+    if(isLoading) {
+      return <Loader />
+    }
+
   return (
     <section className='posts'>
        {posts.length > 0 ? <div className="container posts__container">
           {
-              posts.map(({ id, thumbnail, category, title, desc, authorID }) => 
-                <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} description={desc} authorID={authorID} />
+              posts.map(({ id, thumbnail, category, title, description, authorID, createdAt }) => 
+                <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} description={description} authorID={authorID} createdAt={createdAt} />
               )            
           }
         </div> : <h2 className='center'>No posts found!</h2> }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 import {UserContext} from '../context/UserContext'
+
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const Login = () => {
